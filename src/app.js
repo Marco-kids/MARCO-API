82f5b6d8e4f74c17f1f35888ef4dda6489fe570f
josecas <jmcg0901@gmail.com>
2022-11-15 13:13:17 -0600

    Added CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors")
 const routes = require("./routes/obra");
 require("dotenv").config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use("/api", routes);
 
 // Routes
